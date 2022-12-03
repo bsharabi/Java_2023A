4407b426503f67edec6c3e587d5da00d2fbabe1c
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-03 14:09:32 +0200

    new lesson 5 for evning

--- src/L05/Main.java
+++ src/L05/Main.java
@@ -1,5 +1,26 @@
 package L05;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5};
+        Q1();
+        runningSum(arr);
+      
+    }
+
+    private static void Q1() {
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int sum=0;
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            ans[i]=sum;
+        }
+        return ans;
+    }
 }
